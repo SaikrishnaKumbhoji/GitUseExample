1a1d5118dd9e1f56874d68b3bcea21ada9213925
Sai Kumbhoji <saikrishnak@vs-macmini1.local>
2018-09-20 14:04:58 +0530

    A new map-for-Itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Sai Krishna Kumbhoji on 9/20/18.
+//  Copyright © 2018 Sai Krishna Kumbhoji. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
